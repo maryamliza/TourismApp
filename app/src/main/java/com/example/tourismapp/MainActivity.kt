ee254ce98ce6e0b8a9f962babc85743712aa756f
MaryamLiza <maryam.liza@pucp.pe>
2022-06-15 17:23:23 -0500

    navigation

--- app/src/main/java/com/example/tourismapp/MainActivity.kt
+++ app/src/main/java/com/example/tourismapp/MainActivity.kt
@@ -22,29 +22,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        title = "KotlinApp"
-        tabLayout = findViewById(R.id.tabLayout)
-        viewPager = findViewById(R.id.viewPager)
-        tabLayout.addTab(tabLayout.newTab().setText("Entretenimiento"))
-        tabLayout.addTab(tabLayout.newTab().setText("Cultural"))
-
-
-        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
-        val adapter = MyAdapter(
-            this, supportFragmentManager,
-            tabLayout.tabCount
-        )
-        viewPager.adapter = adapter
-        viewPager.addOnPageChangeListener(TabLayoutOnPageChangeListener(tabLayout))
-        tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                viewPager.currentItem = tab.position
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab) {}
-            override fun onTabReselected(tab: TabLayout.Tab) {}
-        })
+//
+//        title = "KotlinApp"
+//        tabLayout = findViewById(R.id.tabLayout)
+//        viewPager = findViewById(R.id.viewPager)
+//        tabLayout.addTab(tabLayout.newTab().setText("Entretenimiento"))
+//        tabLayout.addTab(tabLayout.newTab().setText("Cultural"))
+//
+//
+//        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+//        val adapter = MyAdapter(
+//            this, supportFragmentManager,
+//            tabLayout.tabCount
+//        )
+//        viewPager.adapter = adapter
+//        viewPager.addOnPageChangeListener(TabLayoutOnPageChangeListener(tabLayout))
+//        tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+//            override fun onTabSelected(tab: TabLayout.Tab) {
+//                viewPager.currentItem = tab.position
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab) {}
+//            override fun onTabReselected(tab: TabLayout.Tab) {}
+//        })
 
 //        //maps
 //        createFragment()
